8f7b2251763f4cbd499c2dacf485f2345e8ec367
teodorstrom <teodorstrom.ts@gmail.com>
2022-03-21 19:29:19 +0000

    add js code for if correct / incorrect answers

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,12 +1,12 @@
 //Get game elements
-const question = document.getElementsByClassName('question-label');
-const choices = document.getElementsByClassName('answer-choice');
-const progressText = document.getElementsByClassName('current-question');
-const scoreText = document.getElementsById('score');
+const question = document.getElementById('question-label');
+const choices = Array.from(document.getElementsByClassName('choice-text'));
+const progressText = document.getElementById('current-question');
+const scoreText = document.getElementById('score');
 
 //Set values for current question and score
 let currentQuestion = {};
-let correctAnswer = true;
+let acceptingAnswer = false;
 let score = 0;
 let questionCounter = [];
 let availableQuestions = [];
@@ -14,50 +14,82 @@ let availableQuestions = [];
 //Choose questions
 let questions = [
     {
-        question: "Where does Norrland's comes from?",
-        choice1: 'Sweden',
-        choice2: 'Norway',
-        choice3: 'Denmark',
-        choice4: 'Finland',
+        question: "WHICH COUNTRY IS THE BIGGEST CONSUMER OF BEER (PER PERSON)?",
+        choice1: 'Austria',
+        choice2: 'Czech Republic',
+        choice3: 'Germany',
+        choice4: 'Romania',
+        answer: 2
+    },
+    {
+        question: "FROM WHICH COUNTRY DOES CORONA BEER ORIGINATE?",
+        choice1: 'Mexico',
+        choice2: 'Spain',
+        choice3: 'Brazil',
+        choice4: 'Dominican Republic',
         answer: 1,
     },
     {
-        question: "Where does Smålands's comes from?",
-        choice1: 'Sweden',
-        choice2: 'Norway',
-        choice3: 'Denmark',
-        choice4: 'Finland',
+        question: "WHERE WAS PILSNER INVENTED?",
+        choice1: 'Czech Republic',
+        choice2: 'Germany',
+        choice3: 'Belgium',
+        choice4: 'Denmark',
         answer: 1,
     },
     {
-        question: "Random question 3",
-        choice1: 'Sweden',
-        choice2: 'Norway',
-        choice3: 'Denmark',
-        choice4: 'Finland',
-        answer: 4,
-    },
-    {
-        question: "This is a another random Q",
-        choice1: 'Sweden',
-        choice2: 'Norway',
-        choice3: 'Denmark',
-        choice4: 'Finland',
+        question: "WHAT IS THE NAME OF GERMANY'S FAMOUS STRONG BEER FESTIVAL?",
+        choice1: 'Oktoberfest',
+        choice2: 'Schreckliches Bierfest',
+        choice3: 'Starkbierfest',
+        choice4: 'Beerapalooza',
         answer: 3,
     },
     {
-        question: "What questions should I use?",
-        choice1: 'Sweden',
-        choice2: 'Norway',
-        choice3: 'Denmark',
-        choice4: 'Finland',
+        question: "WHICH ANCIENT CIVILISATION IS SAID TO HAVE BEEN THE FIRST TO MAKE BEER?",
+        choice1: 'The Babylonians',
+        choice2: 'The Saxons',
+        choice3: 'The Klaxons',
+        choice4: 'The Sumerians',
+        answer: 4,
+    },
+    {
+        question: "WHAT CITY IS THE WORLD'S BIGGEST BEER FESTIVAL HELD IN?",
+        choice1: 'Slough, United Kingdom',
+        choice2: 'Munich, Germany',
+        choice3: 'Prague, Czech Republic',
+        choice4: 'Amsterdam, Netherlands',
         answer: 2,
+    },
+    {
+        question: "WHICH AFRICAN NATION IS FAMOUS FOR BOTH ITS LARGE DESERT AND LOVE FOR DRINKING BEER?",
+        choice1: 'Marocco',
+        choice2: 'Sudan',
+        choice3: 'Algeria',
+        choice4: 'Namibia',
+        answer: 4,
+    },
+    {
+        question: "WHY DO MANY BEERS LOSE FLAVOUR WHEN SERVED COLD?",
+        choice1: 'Cold temperatures dull your tongue',
+        choice2: 'Cold dissolves sugars in the beer',
+        choice3: ' Cold kills the yeast in the beer',
+        choice4: 'Cold makes the bubbles start to oxygenate',
+        answer: 1,
+    },
+    {
+        question: "WHATS THE DEFINING CHARACTERISTIC OF A SESSION BEER?",
+        choice1: "You're only allowed to drink them on stag do's",
+        choice2: 'More hoppy than other beers',
+        choice3: 'Lower alcohol content than other beers',
+        choice4: 'Distinctive fruity flavour',
+        answer: 3,
     }
 ];
 
 
 const SCORE_POINTS = 100;
-const MAX_QUESTIONS = 12;
+const MAX_QUESTIONS = 10;
 
 function startGame() {
     questionCounter = 0;
@@ -65,3 +97,57 @@ function startGame() {
     availableQuestions = [...questions];
     getNewQuestion();
 }
+
+function getNewQuestion() {
+    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score);
+
+        return window.location.assign('/end.html');
+    }
+
+    questionCounter++;
+    progressText.innerText = `${questionCounter} / ${MAX_QUESTIONS}`;
+
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
+
+    choices.forEach(choice => {
+        const number = choice.dataset["number"];
+        choice.innerText =  currentQuestion["choice" + number];
+    });
+
+    availableQuestions.splice(questionIndex, 1);
+    acceptingAnswer: true;
+}
+
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if(!acceptingAnswer) return;
+
+        acceptingAnswer = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset('number');
+
+    
+        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
+
+        if (classToApply === 'correct') {
+            incrementScore(SCORE_POINTS);
+        }
+
+        selectedChoice.parentElement.classList.remove(classToApply);
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply);
+            getNewQuestion();
+        }, 1000)
+    })
+})
+
+incrementScore = num => {
+    score +=num;
+    scoreText.innerText = score;
+}
+
+startGame()

d7ab56b0761777f89b0b691f0c81ca58af95b60f
teodorstrom <teodorstrom.ts@gmail.com>
2022-03-22 18:30:31 +0000

    fix responsive errors in game-area on smaller devices

--- assets/js/game.js
+++ assets/js/game.js
@@ -101,7 +101,7 @@ function getNewQuestion() {
     if(availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
         localStorage.setItem('mostRecentScore', score);
 
-        return window.location.assign('/end.html');
+        return window.location.assign('../end.html');
     }
 
     questionCounter++;
